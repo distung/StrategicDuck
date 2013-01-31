d82d754724655e43e75d844a6d722dcbddadd19c
The Grimace <Nightvoomer@gmail.com>
2013-01-30 20:34:22 -0600

    made position component

---
+++ Game/component/FightStats.py
@@ -0,0 +1,9 @@
+class FighterStats(object):
+    """FighterStats"""
+    def __init___(self, life, speed):
+        self.life = life;
+        self.speed = speed;
+    
+
+
+
---
+++ Game/component/position.py
@@ -0,0 +1,7 @@
+class position(object):
+    """position of the entity"""
+    def __init___(self, x, y):
+        self.x = x;
+        self.y = y;
+
+
