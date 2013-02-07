9d35090bbb3aaa4b51c9d8cded736db30e6493db
The Grimace <Nightvoomer@gmail.com>
2013-02-06 22:58:37 -0600

    added turn system

---
+++ Game/system/turn_system.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class turn_system(object):
+    """description of class"""
+
+    def __init__(self, components={}):
+        self.components = components
+        self.counter = Counter(components.iterkeys)
+
+    def get_active_component(self):
+        while(self.counter.most_common(1)[0][1] < 100):
+            for k, v in dict.iteritems():
+                self.counter[k] += v
+        active = self.counter.most_common(1)[0][0]
+        self.counter[active] = 0
+        return active
+           
+
+    
+
+
