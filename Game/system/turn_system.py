e6cc9746fa10f8b8afa5a5d1001c971169cd51d1
The Grimace <Nightvoomer@gmail.com>
2013-02-09 12:24:42 -0600

    put dict instead of component

--- Game/system/turn_system.py
+++ Game/system/turn_system.py
@@ -7,7 +7,7 @@ class turn_system(object):
 
     def get_active_component(self):
         while(self.counter.most_common(1)[0][1] < 100):
-            for k, v in dict.iteritems():
+            for k, v in components.iteritems():
                 self.counter[k] += v
         active = self.counter.most_common(1)[0][0]
         self.counter[active] = 0
