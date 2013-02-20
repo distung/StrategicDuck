cfb708ab9dcf6d72ed26adcb485dc83c819395cd
The Grimace <Nightvoomer@gmail.com>
2013-02-19 21:00:33 -0600

    added movement system

--- Game/component/FightStats.py
+++ Game/component/FightStats.py
@@ -1,9 +1,9 @@
 class FighterStats(object):
     """FighterStats"""
     def __init___(self,  life, physicalDamage, speed):
-        self.life = life;
-        self.physicalDamage = physicalDamage;
-        self.speed = speed;
+        self.life = life
+        self.physicalDamage = physicalDamage
+        self.speed = speed
     
 
 
